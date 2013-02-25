087cf81d8c11a319bc52b66464f1186e623b201c
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-02-25 11:27:17 +0000

    npm and modules

---
+++ modules/big_module.js
@@ -0,0 +1,5 @@
+module.exports = {
+  makeUrgent: require('./make_urgent'),
+  a: require('./module_a'),
+  pi: Math.PI
+};
---
+++ modules/make_urgent.js
@@ -0,0 +1,4 @@
+module.exports =
+function makeUrgent(s) {
+  return s.toUpperCase() + '!!!';
+};
